58de0588d15542308698ad876805ef4bad42c060
Takuya Namiki <n4m1t4q.dev@gmail.com>
2022-04-01 22:25:04 +0900

    docs: add iam-credentials.json for sls deploy

--- serverless.ts
+++ serverless.ts
@@ -1,14 +1,14 @@
-import type { AWS } from '@serverless/typescript';
-
 import hello from '@functions/hello';
+import type { AWS } from '@serverless/typescript';
 
 const serverlessConfiguration: AWS = {
   service: 'dynamoose-example',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-offline'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: 'ap-northeast-1',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
